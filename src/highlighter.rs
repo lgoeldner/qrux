9306b0a970fbb7394ef460af6d5910a9491d3af0
Linus Göldner <linus@goeldner-family.de>
2024-08-03 19:36:29 +0200

    highlighting

--- src/highlighter.rs
+++ src/highlighter.rs
@@ -1,30 +1,54 @@
-use std::collections::HashSet;
 
-use crate::read;
+use crate::{env::Env, read};
 use nu_ansi_term::{Color, Style};
-use reedline::Highlighter;
-use reedline::StyledText;
+use reedline::{Highlighter, StyledText};
+use std::{cell::RefCell, collections::HashSet, sync::Arc};
 
 pub struct Lisp {
-    funcs: HashSet<&'static str>,
+    funcs: EnvMap,
+    colors: ColorTheme,
+}
+
+pub struct ColorTheme {
+    pub func: Color,
+    pub closure: Color,
+    pub neutral: Color,
+    pub num: Color,
+    pub str: Color,
+    pub bool: Color,
+    pub nil: Color,
+    pub parens: [Color; 3],
+    pub comment: Color,
+    pub kw: Color,
+    pub quoted: Color,
+}
+
+impl Default for ColorTheme {
+    fn default() -> Self {
+        use Color as C;
+        Self {
+            func: C::LightRed,
+            closure: C::LightMagenta,
+            neutral: C::Purple,
+            num: C::Cyan,
+            str: C::LightGreen,
+            bool: C::Blue,
+            nil: C::LightBlue,
+            parens: [
+                Color::Rgb(255, 198, 124),
+                Color::Rgb(228, 124, 255),
+                Color::LightBlue,
+            ],
+            comment: C::DarkGray,
+            kw: Color::Rgb(138, 206, 0),
+            quoted: C::LightGray,
+        }
+    }
 }
 
 impl Highlighter for Lisp {
     fn highlight(&self, line: &str, _cursor: usize) -> StyledText {
-        let func_color = Color::LightRed;
-        let fn_color = Color::LightMagenta;
-        let neutral_color = Color::Purple;
-        let num_color = Color::Cyan;
-        let str_color = Color::LightGreen;
-        let bool_color = Color::Blue;
-        let nil_color = Color::LightBlue;
-        let paren_colors = [
-            Color::Rgb(255, 198, 124),
-            Color::Rgb(228, 124, 255),
-            Color::LightBlue,
-        ];
-        let comment_color = Color::DarkGray;
-        let kw_color = Color::Rgb(138, 206, 0);
+        let colors = &self.colors;
 
         let mut styled_text = StyledText::new();
         let mut tokens = read::tokenize_with_whitespace(line);
@@ -42,7 +66,7 @@ impl Highlighter for Lisp {
                 // skip a quoted list
                 "(" if tokens.prev().is_some_and(|it| it.ends_with('\'')) => {
                     styled_text.push((
-                        Style::from(paren_colors[paren_depth.unsigned_abs() as usize % 3]),
+                        Style::from(colors.parens[paren_depth.unsigned_abs() as usize % 3]),
                         token.to_string(),
                     ));
 
@@ -64,7 +88,7 @@ impl Highlighter for Lisp {
                             break;
                         }
 
-                        styled_text.push((Style::from(Color::LightGray), t.to_string()));
+                        styled_text.push((Style::from(colors.quoted), t.to_string()));
                     }
 
                     tokens.back();
@@ -72,47 +96,47 @@ impl Highlighter for Lisp {
 
                 // parentheses
                 "(" => styled_text.push((
-                    Style::from(paren_colors[paren_depth.unsigned_abs() as usize % 3]),
+                    Style::from(colors.parens[paren_depth.unsigned_abs() as usize % 3]),
                     token.to_string(),
                 )),
 
                 ")" => styled_text.push((
-                    Style::from(paren_colors[(paren_depth + 2).unsigned_abs() as usize % 3]),
+                    Style::from(colors.parens[(paren_depth + 2).unsigned_abs() as usize % 3]),
                     token.to_string(),
                 )),
 
                 kw if kw.starts_with(':') => {
-                    styled_text.push((Style::from(kw_color), token.to_string()));
+                    styled_text.push((Style::from(colors.kw), token.to_string()));
                 }
 
                 string if string.starts_with('"') => {
-                    styled_text.push((Style::from(str_color), token.to_string()));
+                    styled_text.push((Style::from(colors.str), token.to_string()));
                 }
 
                 num if num.parse::<i64>().is_ok() => {
-                    styled_text.push((Style::from(num_color), token.to_string()));
+                    styled_text.push((Style::from(colors.num), token.to_string()));
                 }
 
                 "true" | "false" => {
-                    styled_text.push((Style::from(bool_color), token.to_string()));
+                    styled_text.push((Style::from(colors.bool), token.to_string()));
                 }
 
-                "nil" => styled_text.push((Style::from(nil_color).bold(), token.to_string())),
+                "nil" => styled_text.push((Style::from(colors.nil).bold(), token.to_string())),
 
-                kw if self.funcs.contains(kw) => {
-                    styled_text.push((Style::from(func_color).bold(), token.to_string()));
+                kw if self.env.contains(kw) => {
+                    styled_text.push((Style::from(colors.func).bold(), token.to_string()));
                 }
 
                 comment if comment.starts_with(';') => {
-                    styled_text.push((Style::from(comment_color).bold(), token.to_string()));
+                    styled_text.push((Style::from(colors.comment).bold(), token.to_string()));
                 }
 
                 // non-keyword functions
                 _ if matches!(tokens.prev().map(str::trim), Some("(")) => {
-                    styled_text.push((Style::from(fn_color), token.to_string()));
+                    styled_text.push((Style::from(colors.closure), token.to_string()));
                 }
                 // other data
-                _ => styled_text.push((Style::from(neutral_color), token.to_string())),
+                _ => styled_text.push((Style::from(colors.neutral), token.to_string())),
             }
 
             match trimmed {
@@ -128,15 +152,19 @@ impl Highlighter for Lisp {
 
 impl Lisp {
     /// Construct the default highlighter with a given set of extern commands/keywords to detect and highlight
-    pub fn new(external_commands: Vec<&'static str>) -> Self {
+    pub fn new(
+        env: EnvMap,
+        colors: impl Into<Option<ColorTheme>>,
+    ) -> Self {
         Self {
-            funcs: HashSet::from_iter(external_commands),
+            colors: colors.into().unwrap_or_default(),
+            env,
         }
     }
 }
 
 impl Default for Lisp {
     fn default() -> Self {
-        Self::new(vec![])
+        Self::new(Default::default(), None)
     }
 }
